b4223ce0fd5dd3400c18895fd14182979b4f21a1
Bruno Carneiro <bcarneiro.vermont@alelo.com.br>
2022-03-14 16:13:11 -0300

    inclusao de yml local e criacao de controller, repository e service

--- customer/src/main/java/br/com/cacacontabil/customer/CustomerService.java
+++ customer/src/main/java/br/com/cacacontabil/customer/CustomerService.java
@@ -1,15 +1,19 @@
 package br.com.cacacontabil.customer;
 
-public record CustomerService() {
-    public void registerCustomer(CustomerRegistrationRequest request){
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer( CustomerRegistrationRequest request ) {
         Customer customer = Customer.builder()
-                .firstName(request.firstName())
+                .firstName( request.firstName() )
                 .lastName( request.lastName() )
                 .email( request.email() )
                 .build();
 
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: store customer in db
+
+        customerRepository.save( customer );
     }
 }
